f3dc0601918ccc149823e0340a8dbd2d688e61c0
ShahzaibSE <shahzaibanalyst@outlook.com>
2019-07-17 17:03:02 +0500

    Added user routes.

--- restaurant-server/routes/users.ts
+++ restaurant-server/routes/users.ts
@@ -4,25 +4,21 @@ import {UserController} from "./../controllers/user-controller"
 export class UserRoutes {      
     public userController = new UserController()
     public routes(app): void {          
-        app.route('/user')
-        .get((req: Request, res: Response) => {            
-            res.status(200).send({
-                status: true,
-                resCode: 200,
-                isError: false,
-                message: "User fetched successfully"
-            })
-        }).post(this.userController.addNewContact)  // Adding user
+        // app.route('/user')
+        // .get((req: Request, res: Response) => {            
+        //     res.status(200).send({
+        //         status: true,
+        //         resCode: 200,
+        //         isError: false,
+        //         message: "User fetched successfully"
+        //     })
+        // }).post(this.userController.addNewContact)  // Adding user
+
+        app.route("/user/login").post(this.userController.loginIn)
+        app.route("/user/signup").post(this.userController.createUser)        
           
         // Edit user.
-        app.route("/user/:uid").put((req: Request, res: Response) => {
-            res.status(201).send({
-                status: true,
-                resCode: 201,
-                isError: false,
-                message: "User updated successfully"
-            })
-        }).delete((req: Request, res: Response) => {
+        app.route("/user/:uid").put(this.userController.update).delete((req: Request, res: Response) => {
             res.status(202).send({
                 status: true,
                 resCode: 201,
